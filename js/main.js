a0d4cc26a9f7c5a7fe70a5de06c31a7d5bbf2067
JanAksamit <76951669+JanAksamit@users.noreply.github.com>
2021-01-13 18:51:10 +0200

    commit all

---
+++ js/main.js
@@ -0,0 +1,142 @@
+
+
+// vienos eilute komentara
+
+/*
+
+
+keletos
+eiluciu
+komentaras
+*/
+
+console.log(63514654645)
+
+console.log("labutis")
+
+
+
+/*
+Reiksmiu tipai:
+
+number
+string -trkstas
+boolean - logine reiksme
+array- sarasas
+object- objektas
+*/
+
+/* kintamuju inicijavimo budai:
+const - pagrindinis, kuri reikia rinktis by default
+let - renkmes, jei info keiciasi
+var - nenaudoti
+
+//inicijuoti kintamaji, kuris niekada nesikeis ir bus skaicius 2021*/
+const metai = 2021;
+
+// inicijuoti k intamaji, kuris niekasa nesikeis ir bus tekstas Petras*/
+const vardas = "Petras"  
+
+console.log(metai)
+console.log(vardas)
+
+console.log(metai, vardas)
+
+console.log('Metai', metai)
+
+
+
+const pirmasSkaicius = 13;
+const antrasSkaicius = 2;
+
+
+const suma = pirmasSkaicius + antrasSkaicius
+
+console.log(suma)
+
+let adresas = 'Vilnius';
+console.log(adresas)
+
+adresas = 'Kaunas'
+console.log(adresas)
+
+adresas = 'Klaipeda';
+console.log(adresas)
+
+let pinigine = 0;
+console.log('Turiu pinigu', pinigine)
+
+const pirmaAlga = 100;
+pinigine = pinigine + pirmaAlga;
+console.log('Turiu pinigu', pinigine);
+
+const antraAlga =  250;
+pinigine = pinigine + antraAlga;
+console.log('Turiu pinigu', pinigine);
+
+
+const treciaAlga = 660;
+pinigine = pinigine + treciaAlga;
+console.log('Turiu pinigu', pinigine);
+
+console.log('----------------------');
+
+const s1 = 5;
+const s2 = 8;
+
+console.log(s1 + s2);
+console.log(s1 - s2);
+console.log(s1 * s2);
+console.log(s1 / s2);
+console.log(s1 ** s2);
+
+// tekstines operacijos
+
+
+const hi = 'Labas';
+const morning = 'rytas';
+
+const sayHi = hi + ' ' +  morning + '!';
+console.log(sayHi);
+
+
+/* 
+teksto reiksme gali buti inicijuota:
+    viengubuose kabutese
+    dvigubuose kabutese
+    */
+//tekste naudojamos dvigubos kabutes
+
+    const dvigubosTekste = 'Tekste naudojamos dvigubos (") kabutes.';
+    console.log(dvigubosTekste)
+
+
+    //tekste naudojamos viengubos kabutes
+    const viengubosTekste = "Tekste naudojamos viengubos (') kabute";
+    console.log(viengubosTekste);
+
+
+
+// tekste naudojamos viengubos kabutes
+const viengubosTekste = "Tekste naudojamos viengubos (') kabutes.";
+console.log(viengubosTekste);
+
+// tekste naudojamos viengubos kabutes ir dvigubos kabutes
+const abiTekste1 = 'Tekste yra abi kabutes 1 (\') ir 2(").';
+console.log(abiTekste1);
+
+const abiTekste2 = "Tekste yra abi kabutes 1 (') ir 2(\").";
+console.log(abiTekste2);
+
+const abiTekste12 = 'Tekste yra abi kabutes 1 (\') ir 2(\").';
+console.log(abiTekste12);
+
+const abiTekste21 = "Tekste yra abi kabutes 1 (\') ir 2(\").";
+console.log(abiTekste21);
+
+
+    const html = '<div class="hi>Labas rytas</div>';
+    console.log(html)
+
+    
+
